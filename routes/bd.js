440c84c2dafa68efb0ab8f619366db6e155861fc
jassoLuz <jassolucii@gmail.com>
2021-10-19 20:33:00 -0500

    Update bd.js

--- routes/bd.js
+++ routes/bd.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 
 var conexion=mysql.createConnection({
-    host:'127.0.0.1',
-    user:'root',
-    password:'',
-    database:'sistema',
+    host:'us-cdbr-east-04.cleardb.com',
+    user:'baec3702f459d5',
+    password:'57597920',
+    database:'heroku_10ff34f8473daa0',
     port:'3306'
 });
 
